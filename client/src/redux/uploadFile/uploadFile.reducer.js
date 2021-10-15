9836635ce3328089ef2a27553f6174db2d159bda
mdestafadilah <desta.08b@gmail.com>
2021-10-15 12:24:59 +0700

    utilitas

--- client/src/redux/uploadFile/uploadFile.reducer.js
+++ client/src/redux/uploadFile/uploadFile.reducer.js
@@ -1,4 +1,5 @@
 import uploadFileTypes from "./uploadFile.types";
+import { modifyFiles } from "./uploadFile.utils";
 
 const INITIAL_STATE = {
     fileProgress: {
@@ -6,13 +7,14 @@ const INITIAL_STATE = {
     }
 }
 
-const fileProgressReducer = (state = INITIAL_STATE, action) => {
+const fileProgressReducer = (state = INITIAL_STATE, action) => { //console.log(action)
     switch (action.type) {
         case uploadFileTypes.SET_UPLOAD_FILE:
             return {
                 ...state,
                 fileProgress: {
                     ...state.fileProgress,
+                    ...modifyFiles(state.fileProgress, action.payload)
                 },
             }
     
