f5281c625f86d46e8d98eb2fdeaf2cf790ad6f9a
mdestafadilah <desta.08b@gmail.com>
2021-10-15 11:09:55 +0700

    ROOT REDUCERS

---
+++ client/src/redux/root-reducer.js
@@ -0,0 +1,8 @@
+import { combineReducers } from 'redux'
+import UplodFile from './uploadFile/uploadFile.reducer'
+
+const rootReducer = combineReducers({
+    UplodFile,
+})
+
+export default rootReducer
