41c2809c7c2a99b6e5640d26aa17c68bcb6a1ff0
mdestafadilah <desta.08b@gmail.com>
2021-10-15 11:10:22 +0700

    INTEGRATED REDUX TO APP

--- client/src/index.js
+++ client/src/index.js
@@ -1,13 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware, compose } from 'redux';
+import thunk from 'redux-thunk'
+
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import rootReducer from './redux/root-reducer';
+
+const middleware = [thunk]
+const composeEnchancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose 
+const store = createStore(rootReducer, composeEnchancers(applyMiddleware(...middleware)))
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
+  </Provider>,
   document.getElementById('root')
 );
 
